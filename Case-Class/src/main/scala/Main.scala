779c43b694a72150b8e02a167e619c6925d58e88
Subhrajit-Bigdata <67468091+Subhrajit-Bigdata@users.noreply.github.com>
2020-06-29 11:57:05 +0530

    Update Main.scala

--- Case-Class/src/main/scala/Main.scala
+++ Case-Class/src/main/scala/Main.scala
@@ -1,9 +1,9 @@
 object Main extends App {
-  val persons = List(
-    Person(firstName = "Biswajit", lastName = "Acharya", age = 12),
-    Person(firstName = "Satyajit", lastName = "Mohapatra", age = 34),
-    Person(firstName = "Mir", lastName = "Abdul", age = 52))
-  val adults = Person.filterAdult(persons)
+  val peoples = List(
+    People(firstName = "Biswajit", lastName = "Acharya", age = 12),
+    People(firstName = "Satyajit", lastName = "Mohapatra", age = 34),
+    People(firstName = "Mir", lastName = "Abdul", age = 52))
+  val adults = Person.filterAdult(peoples)
   val descriptions = adults.map(p => p.description).mkString("\n\t")
   println(s"The adults are \n\t$descriptions")
 }
