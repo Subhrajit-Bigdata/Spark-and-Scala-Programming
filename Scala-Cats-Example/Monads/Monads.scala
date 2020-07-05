4c8833b571d4b7c69feab0ea1b21bc660b62a0dc
Subhrajit-Bigdata <67468091+Subhrajit-Bigdata@users.noreply.github.com>
2020-07-06 01:10:41 +0530

    Create Monads.scala

---
+++ Scala-Cats-Example/Monads/Monads.scala
@@ -0,0 +1,31 @@
+object Monads extends App {
+
+  trait Attempt[+A]{
+    def flatMap[B](f:A => Attempt[B]): Attempt[B]
+  }
+  object Attempt{
+    def apply[A](a: => A): Attempt[A] =
+      try {
+        Success(a)
+      }catch {
+        case e: Throwable => Fail(e)
+      }
+  }
+  case class Success[+A](value: A) extends Attempt[A]{
+    def flatMap[B](f:A => Attempt[B]): Attempt[B] =
+      try{
+        f(value)
+      }catch {
+        case e: Throwable => Fail(e)
+      }
+  }
+  case class Fail(e:Throwable) extends Attempt[Nothing]{
+    println("step3a....")
+    def flatMap[B](f:Nothing => Attempt[B]): Attempt[B] = this
+  }
+  val attempt = Attempt{
+    throw new RuntimeException("My own monad")
+  }
+  println(attempt)
+
+}
